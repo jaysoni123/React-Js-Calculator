0dd0fa97e445ef4894b48036cd9bc1ba73ceeaea
Dhananjay Choksi <soni.jay008@gmail.com>
2020-07-02 11:05:15 +0530

    Updates

---
+++ precache-manifest.af1402d2bcbb58a352d9e9a9e34c0401.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380255d5d6875b072f2cfb3114b95840",
+    "url": "/React-Js-Calculator/index.html"
+  },
+  {
+    "revision": "2ab221cc7357a8544a8a",
+    "url": "/React-Js-Calculator/static/css/main.0886e170.chunk.css"
+  },
+  {
+    "revision": "af92100a21722c7d4592",
+    "url": "/React-Js-Calculator/static/js/2.b3f26618.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Js-Calculator/static/js/2.b3f26618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ab221cc7357a8544a8a",
+    "url": "/React-Js-Calculator/static/js/main.396f661e.chunk.js"
+  },
+  {
+    "revision": "5af9230796093349567c",
+    "url": "/React-Js-Calculator/static/js/runtime-main.82b17d98.js"
+  }
+]);
